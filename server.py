6be6c796b406d8a571065443f2e2b4e9d4fed1bf
Quema <105044971+Quema100@users.noreply.github.com>
2023-05-16 00:59:57 +0900

    Update server.py

--- server.py
+++ server.py
@@ -8,8 +8,8 @@ CORS(app)
 @app.route('/log', methods=['POST'])
 def log():
     with open('log.txt', 'a') as f:
-        f.write(f"datetime:|{datetime.now()}|--------------"+request.data.decode() + '\n')
+        f.write(f"datetime:|{datetime.now()}|--------------"+request.data.decode() + '\n') # 날짜와 json형식으로 받은 데이터 .txt 파일로 변환
     return '', 204
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080 ,debug=True,ssl_context='adhoc')
+    app.run(host='0.0.0.0', port=8080 ,debug=True,ssl_context='adhoc') #ssl 임시 인증
